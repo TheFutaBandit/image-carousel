4ebf6c4a8252598d34a7bbc14f4dfca7789c514b
TheFutaBandit <prameshanand2021@gmail.com>
2024-10-16 13:30:01 +0530

    Basic HTML skeleton, Javascript Application logic and working on Javascript display logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-console.log("hi");
+import './styles.css';
+
+const slideShow = (totalSlides) => {
+    let slideIndex = 0;
+    totalSlides = totalSlides;
+
+    function mod(n, m) {
+        return ((n % m) + m) % m;
+    }
+
+    const incrementSlide = () => {
+        slideIndex = mod(slideIndex+1,totalSlides);
+    }
+
+    const decrementSlide = () => {
+        slideIndex = mod(slideIndex-1,totalSlides);
+    }
+
+    const getSlide = () => {
+        return slideIndex;
+    }
+
+    return {
+        incrementSlide, 
+        decrementSlide,
+        getSlide
+    };
+}
+
+const slideShowDisplayModule = () => {
+    const displaySlide = (slideNode) => {
+        slideNode.classList.toggle("visible");
+    }
+
+    return {
+        displaySlide,
+    }
+}
+
+
+
+
+const webpageLoader = (() => {
+    const carousel = slideShow(3);
+    const displayModule = slideShowDisplayModule();
+
+    const picture_one = document.querySelector(".picture_one");
+    const picture_two = document.querySelector(".picture_two");
+    const picture_three = document.querySelector(".picture_three");
+
+    const resetSlide = () => {
+        [picture_one,picture_two,picture_three].forEach((item) => {
+            if(item.classList.toggle("visible") === true) {
+                item.classList.toggle("visible");
+            }
+        })
+    }
+
+    const nextSlide = (() => {
+        const nextButton = document.querySelector(".next_arrow");
+        nextButton.addEventListener("click", () => {
+            resetSlide();
+            displayModule.displaySlide(picture_two);
+        })
+    })();
+
+    console.log(carousel.getSlide());
+})();
