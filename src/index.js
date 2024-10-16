37f3a11ec24de3cf0aac1695750e7f80f584c23b
TheFutaBandit <prameshanand2021@gmail.com>
2024-10-16 14:22:56 +0530

    added DotSwitching() Functionality, added setSlide() in the process

--- src/index.js
+++ src/index.js
@@ -20,10 +20,15 @@ const slideShow = (totalSlides) => {
         return slideIndex;
     }
 
+    const setSlide = (index) => {
+        slideIndex = index;
+    }
+
     return {
         incrementSlide, 
         decrementSlide,
-        getSlide
+        getSlide,
+        setSlide
     };
 }
 
@@ -32,8 +37,13 @@ const slideShowDisplayModule = () => {
         slideNode.classList.toggle("visible");
     }
 
+    const displayDot = (dotNode) => {
+        dotNode.classList.toggle("visible_dot");
+    }
+
     return {
         displaySlide,
+        displayDot
     }
 }
 
@@ -48,19 +58,83 @@ const webpageLoader = (() => {
     const picture_two = document.querySelector(".picture_two");
     const picture_three = document.querySelector(".picture_three");
 
+    const dot_one = document.querySelector(".dot_one");
+    const dot_two = document.querySelector(".dot_two");
+    const dot_three = document.querySelector(".dot_three");
+
+    const carouselArray = [picture_one,picture_two,picture_three];
+
+    const dotArray = [dot_one,dot_two,dot_three];
+
+    const dotArrayValue = ["dot_one","dot_two","dot_three"];
+
     const resetSlide = () => {
-        [picture_one,picture_two,picture_three].forEach((item) => {
+        carouselArray.forEach((item) => {
             if(item.classList.toggle("visible") === true) {
                 item.classList.toggle("visible");
             }
         })
     }
 
+    const resetDot = () => {
+        dotArray.forEach((item) => {
+            if(item.classList.toggle("visible_dot") === true) {
+                item.classList.toggle("visible_dot");
+            }
+        })
+    }
+
+    const defaultSlideLoad = (() => {
+        let index = carousel.getSlide();
+        let slideNode = carouselArray[index];
+        let dotNode = dotArray[index];
+        resetSlide();
+        resetDot();
+        displayModule.displaySlide(slideNode);
+        displayModule.displayDot(dotNode);
+    })();
+
     const nextSlide = (() => {
         const nextButton = document.querySelector(".next_arrow");
         nextButton.addEventListener("click", () => {
+            carousel.incrementSlide();
+            let index = carousel.getSlide();
+            let slideNode = carouselArray[index];
+            let dotNode = dotArray[index];
             resetSlide();
-            displayModule.displaySlide(picture_two);
+            resetDot();
+            displayModule.displaySlide(slideNode);
+            displayModule.displayDot(dotNode);
+        })
+    })();
+
+    const prevSlide = (() => {
+        const prevSlide = document.querySelector(".prev_arrow");
+        prevSlide.addEventListener("click", () => {
+            carousel.decrementSlide();
+            let index = carousel.getSlide();
+            let slideNode = carouselArray[index];
+            let dotNode = dotArray[index];
+            resetSlide();
+            resetDot();
+            displayModule.displaySlide(slideNode);
+            displayModule.displayDot(dotNode);
+        })
+    })();
+
+    const dotSwitching = (() => {
+        dotArray.forEach((item) => {
+            item.addEventListener("click", () => {
+                let index = dotArrayValue.indexOf(item.dataset.name);
+                resetSlide();
+                resetDot();
+                carousel.setSlide(index);
+                let newIndex = carousel.getSlide();
+                let slideNode = carouselArray[index];
+                let dotNode = dotArray[index];
+                displayModule.displaySlide(slideNode);
+                displayModule.displayDot(dotNode);
+            })
         })
     })();
 
